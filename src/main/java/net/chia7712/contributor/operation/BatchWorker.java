b0c4ae48dc6a38a263b17064d1408be63569488f
ChiaPing Tsai <chia7712@gmail.com>
2016-12-29 23:58:32 +0800

    remove unused count

--- src/main/java/net/chia7712/contributor/operation/BatchWorker.java
+++ src/main/java/net/chia7712/contributor/operation/BatchWorker.java
@@ -1,12 +1,9 @@
 package net.chia7712.contributor.operation;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Durability;
 import org.apache.hadoop.hbase.client.Increment;
@@ -20,12 +17,6 @@ class BatchWorker implements Slave {
   private static final int BATCH_VALUE = 10;
   private final List<Row> rows = new LinkedList<>();
   private final Random rm = new Random();
-  private final Map<Type, AtomicInteger> count = new HashMap<>();
-  BatchWorker() {
-    for (Type type : Type.values()) {
-      count.put(type, new AtomicInteger(0));
-    }
-  }
   private int flush(final Table table) throws IOException, InterruptedException {
     if (!rows.isEmpty()) {
       table.batch(rows);
@@ -37,7 +28,6 @@ class BatchWorker implements Slave {
   }
   @Override
   public int finish(final Table table) throws IOException, InterruptedException {
-    count.forEach((k, v) -> System.out.println(k + ":" + v));
     return flush(table);
   }
   @Override
@@ -67,7 +57,6 @@ class BatchWorker implements Slave {
       default:
         throw new RuntimeException("Unsupported type:" + type);
     }
-    count.get(type).incrementAndGet();
     if (rows.size() >= limit) {
       return flush(table);
     }
